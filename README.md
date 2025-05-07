2797f1312ed0baffa30a0162483e6a7bb69a2b49
alperenkursun <alperenkursun@outlook.com>
2025-05-07 17:26:35 +0300

    initial commit & create apollo server

---
+++ README.md
@@ -0,0 +1,11 @@
+# countries-graphql-api
+
+GraphQL Exercise
+
+---
+
+<!-- ![cgaimg](cga.png) -->
+
+[Patika GraphQL Project-1](https://academy.patika.dev/courses/graphql/project-01)
+
+[Patika](https://academy.patika.dev/tr/@alpk)
---
+++ index.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { books } from "./data.js";
+
+const typeDefs = `#graphql
+
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
